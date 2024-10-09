2a1e60e6d529bf348af8f32e75dac5f95bf49ae2
flip <dmobl30@wgu.edu>
2024-10-08 20:44:48 -0500

    added report function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
     num_words = get_num_words(text)
     low_text = get_lowercased(text)
     char_count = count_characters(low_text)
+    report = run_count_report(book_path, num_words, char_count)
 
 def get_num_words(text):
     words = text.split()
@@ -24,8 +25,15 @@ def count_characters(text):
     for char in text.lower():  # Assuming text is already lowercase
         if char.isalpha():
             char_count[char] = char_count.get(char, 0) + 1
-    sorted_char_count = dict(sorted(char_count.items()))
-    return sorted_char_count
+    sorted_items = sorted(char_count.items(), key=lambda item: item[1], reverse=True)
+    return sorted_items
+
+def run_count_report(book_path, num_words, char_count):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+    for char, count in char_count:
+        print(f"The '{char}' character was found {count} times.")
 
 
 main()
