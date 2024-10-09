ffa206423e5e56a11694463160698b0a27d57528
flip <dmobl30@wgu.edu>
2024-10-08 19:52:43 -0500

    updated main.py, added word count function, updated read function

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import os
 
 def main():
-    path_to_file = "books/frankenstein.txt"
-    #print("Current working directory:", os.getcwd())
-    #print("'books' directory exists:", os.path.exists("books"))
-    #print("Files in 'books' directory:", os.listdir("books"))
-    with open(path_to_file) as f:
-        file_contents = f.read()
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
