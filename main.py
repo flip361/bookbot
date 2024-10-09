31f2f235249e55976d5c7a3a2571766cfd6beae0
flip <dmobl30@wgu.edu>
2024-10-08 20:16:36 -0500

    added a character count function, returns count alphabetically

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    low_text = get_lowercased(text)
+    char_count = count_characters(low_text)
 
 def get_num_words(text):
     words = text.split()
@@ -12,6 +14,18 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_lowercased(text):
+    lowered_text = text.lower()
+    return lowered_text
+
+def count_characters(text):
+    char_count = {}
+    for char in text.lower():  # Assuming text is already lowercase
+        if char.isalpha():
+            char_count[char] = char_count.get(char, 0) + 1
+    sorted_char_count = dict(sorted(char_count.items()))
+    return sorted_char_count
 
 
 main()
